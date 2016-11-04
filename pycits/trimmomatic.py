8e6eb0839884c59e4bdfbec08fb7aaf3bdb9c3b6
Peter Thorpe <peter.thorpe@hutton.ac.uk>
2016-11-04 15:49:05 +0000

    working trimmo scripts failled pep8

--- pycits/trimmomatic.py
+++ pycits/trimmomatic.py
@@ -11,14 +11,16 @@
 import os
 import sys
 
-from subprocess import Popen, PIPE
+import subprocess
+#from .tools import is_exe
 
 
 class Trimmomatic(object):
     """Class for working with trimmomatic"""
-    def __init__(self, exe_path, logger):
+    def __init__(self, exe_path, logger=False):
         """Instantiate with location of executable"""
-        self._logger = logger
+        if logger:
+            self._logger = logger
         self._no_run = False
         if not os.path.isfile(exe_path):
             # TO DO: return this error message when it fails
@@ -32,25 +34,23 @@ class Trimmomatic(object):
                    "in your PATH ",
                    " you can download the binaries from ",
                    "http://www.usadellab.org/cms/?page=trimmomatic"])
-
-            self._logger.warning(msg)
+            if logger:
+                self._logger.warning(msg)
             self._no_run = True
         self._exe_path = exe_path
 
     def __build_cmd(self, trimmo_prog, L_reads, R_reads, threads,\
-                    outdir, HEADCROP):
+                    outdir, HEADCROP=0, logger=False):
         """Build a command-line for trimmomatic"""
-        if int(HEADCROP) > 0:
-            self._logger.info("left crop reads at %d bases" %
-                              int(HEADCROP))
+        if logger:
+            if int(HEADCROP) > 0:
+                self._logger.info("left crop reads at %d bases" %
+                                  int(HEADCROP))
         # trimmo can trim the start of reads. Default this will be 0
         # but this can be defined by the user. 
         HEADCROP = "HEADCROP:%s" %(str(HEADCROP))
         prefix = L_reads.split("_R")[0]
         prefix = prefix.split("/")[-1]
-        print prefix
-        print trimmo_prog, L_reads, R_reads, threads,\
-                    outdir, HEADCROP
         self._outdirname = os.path.join(outdir)
         self._Left_outfile = os.path.join(outdir,
                             prefix + "_paired_R1.fq.gz")
@@ -74,7 +74,7 @@ class Trimmomatic(object):
 
 
     def run(self, trimmo_prog, L_reads, R_reads, threads, \
-            outdir, HEADCROP="0"):
+            outdir, HEADCROP="0", logger=None):
         """Run trimmomatic on the passed read files"""
         assert L_reads != R_reads, """Oh no,
         I am trying to perform trimming on two files that are the same!
@@ -83,14 +83,23 @@ class Trimmomatic(object):
         self.__build_cmd(trimmo_prog, L_reads, R_reads, threads, \
                          outdir, HEADCROP)
         if not os.path.exists(self._outdirname):
-            self._logger.info("Creating output directory: %s" %
+            if logger:
+                self._logger.info("Creating output directory: %s" %
                               self._outdirname)
             os.makedirs(self._outdirname)
         msg = ["Running...", "\t%s" % self._cmd]
-        for m in msg:
-            self._logger.info(m)
-        pipe = Popen(self._cmd, shell=True, stdout=PIPE)
-        if pipe.wait() != 0:
-            self._logger.error("trimmomatic generated some errors")
+        #for m in msg:
+        #    self._logger.info(m)
+        pipe = subprocess.run(self._cmd, shell=True, \
+                            stdout=subprocess.PIPE, \
+                            stderr=subprocess.PIPE, \
+                            check=True)
+        if pipe.returncode != 0:
+            if logger:
+                self._logger.error("trimmomatic generated some errors")
             sys.exit(1)
-        return (self._outdirname, pipe.stdout.readlines())
+        if pipe.returncode == 0:
+            if logger:
+                self._logger.info(pipe)
+        print ("pipe.args = ", pipe.args)
+        return (self._outdirname, pipe.args)
