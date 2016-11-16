bc63f2b8c53f03ef277d80b4ba23248173b65902
widdowquinn <leighton.pritchard@hutton.ac.uk>
2016-11-16 11:50:18 +0000

    make PEP8 changes to module_tester.py

--- module_tester.py
+++ module_tester.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 # THIS IS a tester for module dev only.
+#
+# TODO - put this functionality under a proper test framework (e.g. nosetests
+#        or equivalent)
+
 
 import errno
 import logging
@@ -25,9 +29,8 @@ read_prefix = left_reads.split("_R")[0]
 read_prefix = read_prefix.split("/")[-1]
 
 # left_trim 53 - this should be default??
-
 left_trim = 53
-right_trim = 0 
+right_trim = 0
 
 
 # Report last exception as string
@@ -40,8 +43,6 @@ def last_exception():
 
 # Run as script
 if __name__ == '__main__':
-
-
     # Set up logging
     logger = logging.getLogger('thapbi_santi_otus.py: %s' % time.asctime())
     logger.setLevel(logging.DEBUG)
@@ -67,16 +68,16 @@ if __name__ == '__main__':
     logger.info("Starting testing: %s" % time.asctime())
 
     # trimmomatic testing.
-    trimmo_prog = "/home/pt40963/scratch/Downloads/Trimmomatic-0.36/trimmomatic-0.36.jar"
-    
+    trimmo_prog = "/home/pt40963/scratch/Downloads/Trimmomatic-0.36/" +\
+                  "trimmomatic-0.36.jar"
+
     logger.info("starting trimmomatic testing")
     trim = trimmomatic.Trimmomatic(trimmo_prog, logger)
-    
+
     logger.info("Trim reads by quality")
     trim.run(trimmo_prog, left_reads, right_reads,
              threads, "trimmed_reads", 0, logger)
-    
-###################################################################################################
+
     # error correction testing
     ByHam = "/home/pt40963/scratch/Downloads/SPAdes-3.9.0-Linux/bin/spades.py"
     error_correct = error_correction.Error_correction(ByHam,
@@ -98,13 +99,12 @@ if __name__ == '__main__':
                          read_prefix + "_paired_R2.fq" +
                          ".00.0_0.cor.fastq.gz")
 
-###################################################################################################
     # PEAR testing - assemble
     assemble = pear.Pear("pear", logger)
     Left_trimmed = os.path.join("trimmed_reads",
-                            read_prefix + "_paired_R1.fq.gz")
+                                read_prefix + "_paired_R1.fq.gz")
     Right_trimmed = os.path.join("trimmed_reads",
-                            read_prefix + "_paired_R2.fq.gz")
+                                 read_prefix + "_paired_R2.fq.gz")
     logger.info("assembly using PEAR")
     assemble.run(Left_trimmed, Right_trimmed,
                  threads, "PEAR_assembled", logger)
@@ -113,30 +113,26 @@ if __name__ == '__main__':
     logger.info("assembly using PEAR with error corrected reads")
     assemble.run(L_E_C, R_E_C,
                  threads, "PEAR_assembled_EC", logger)
-    
-###################################################################################################
+
     # cleaning up unwanted files
     logger.info("testing the removal of unwanted file")
     unwanted_list = ["unpaired_R*.fq.gz",
                      "PEAR_assembled/*.discarded*fastq",
                      "PEAR_assembled/*.unassembled*fastq"]
-                     
+
     delete_file = clean_up.Clean_up("rm", logger)
     for i in unwanted_list:
         delete_file.run(i)
 
-###################################################################################################
-# convert format
+    # convert format
     format_change = seq_crumbs.Convert_Format("convert_format",
                                               logger)
     assembled_reads = os.path.join("PEAR_assembled",
-                    read_prefix+ "_paired_PEAR.assembled.fastq")
+                                   read_prefix +
+                                   "_paired_PEAR.assembled.fastq")
 
     format_change.run(assembled_reads, "fasta_converted",
                       logger)
-
-
-##################################################################################################
 
     # deduplicate read:
     #python deduplicate_rename.py
@@ -153,17 +149,16 @@ if __name__ == '__main__':
     dedup.run(dedup_prog, fasta_file, database, out_dedup,
               logger)
 
-##################################################################################################
     # need to trim the left and right assembled seq so they
     # cluster with the database.
-
     left_trim = 53
     right_trim = 0
     chop_prog = os.path.join("pycits", "trim_fasta_file.py")
 
     fasta_file = os.path.join("fasta_converted", "temp.fasta")
     ITS_database = os.path.join("data",
-                                "ITS_database_NOT_confirmed_correct_last14bases_removed.fasta")
+                                "ITS_database_NOT_confirmed_correct_" +
+                                "last14bases_removed.fasta")
     fasta_out = os.path.join("fasta_converted",
                              "database_assembled_reads.fasta")
 
@@ -172,10 +167,7 @@ if __name__ == '__main__':
             #fasta_out, barcode="0", logger=False)
     chopper.run(chop_prog, fasta_file, ITS_database, left_trim,
                 right_trim, fasta_out, logger)
-    
 
-
-########################################################################
     # SWARM testing - assemble
     cluster = swarm.Swarm("swarm", logger)
     assembled_fa_reads = os.path.join("fasta_converted",
@@ -183,14 +175,12 @@ if __name__ == '__main__':
 
     logger.info("clustering with Swarm")
     cluster.run(assembled_fa_reads,
-                 threads, 1, "Swarm_cluster_assembled_reads_only", logger)
+                threads, 1, "Swarm_cluster_assembled_reads_only", logger)
     cluster.run(fasta_out,
-                 threads, 1, "Swarm_cluster", logger)
-                                    
+                threads, 1, "Swarm_cluster", logger)
 
-##########################################################################
     # recode cluster output
-    #python pycits/parse_clusters_new_to_old_name.py -i 
+    #python pycits/parse_clusters_new_to_old_name.py -i
     # -i swarm_tempd1.out -d database.out -o decoded_clusters.out
     logger.info("renaming swarm output")
     rename_prog = os.path.join("pycits",
@@ -199,13 +189,9 @@ if __name__ == '__main__':
     infile = os.path.join("Swarm_cluster", "swarm_clustering_d1",
                           "swarm_clustering_d1")
     outfile = os.path.join("Swarm_cluster", "swarm_clustering_d1",
-                          "swarm_clustering_d1_renamed.out")
+                           "swarm_clustering_d1_renamed.out")
     rename.run(rename_prog, infile, ITS_database,
                database, outfile, logger)
 
-##########################################################################
     # need to summarise the clusters now
     # how? scripts already made, are they good enough?
-                                      
-
-    
