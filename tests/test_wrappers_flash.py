8a9899799b55c40f6e97a9e457e1ff90f2d24606
Peter Thorpe <peter.thorpe@hutton.ac.uk>
2016-12-15 15:01:19 +0000

    testing script and files nto yet passing

--- tests/test_wrappers_flash.py
+++ tests/test_wrappers_flash.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-"""Tests of wrapper code in pycits. Trimmomatic"""
+"""Tests of wrapper code in pycits: Flash"""
 
 import os
 import shutil
@@ -10,82 +10,60 @@ from pycits.tools import NotExecutableError
 
 from nose.tools import nottest, assert_equal
 
+# INPUT DATA LOCATION
 INDIR = os.path.join("tests", "test_data")
 OUTDIR = os.path.join("tests", "test_out_flash")
+READS1 = os.path.join(INDIR, "DNAMIX_S95_L001_R1_001.fastq.gz")
+READS2 = os.path.join(INDIR, "DNAMIX_S95_L001_R2_001.fastq.gz")
+PREFIX = os.path.split(READS1)[-1].split("_R")[0]
 
-# this will depend on the binarie name of flash
-# WE will need to change this!!
-
+# TARGET OUTPUT DATA
+TARGET = os.path.join("tests", "test_targets", "flash",
+                      "flash_test.assembled.fastq")
 
 def test_flash():
     """flash instantiates with cmd-line if flash is in $PATH"""
     assemble = flash.Flash("flash")
 
-
 def test_flash_cmd():
-    """flash instantiates and returns correct form of cmd-line"""
-    assembl = flash.Flash("flash")
-    prefix = "DNAMIX_S95_L001"
-    outdirname = os.path.join("tests/test_out_flash", "%s_flash"
-                              % (prefix))
-    command = ["flash",
-               "--max-overlap", "250",
-               "--threads",
-               "4",
-               "-o",
-               outdirname,
-               "tests/test_data/DNAMIX_S95_L001_R1_001.fastq.gz",
-               "tests/test_data/DNAMIX_S95_L001_R2_001.fastq.gz",]
-    target = ' '.join(command)
-    assert_equal(assembl.run("tests/test_data/DNAMIX_S95_L001_R1_001.fastq.gz",
-                             "tests/test_data/DNAMIX_S95_L001_R2_001.fastq.gz",
-                             4, "tests/test_out_flash"), target)
-
+    """flash instantiates, runs and returns correct form of cmd-line"""
+    obj = flash.Flash("flash")
+    target = ' '.join(["flash", "--max-overlap", "250",
+                       "--threads", "4", "-o", os.path.join(OUTDIR, PREFIX),
+                       READS1, READS2])
+    assert_equal(obj.run(READS1, READS2, 4, OUTDIR, PREFIX, dry_run=True),
+                 target)
 
 def test_flash_exec_notexist():
     """Error thrown if flash executable does not exist"""
     try:
-        assemble = flash.Flash(os.path.join(".", "flash"))
+        obj = flash.Flash(os.path.join(".", "flash"))
     except NotExecutableError:
         return True
     else:
         return False
-
 
 def test_flash_notexec():
     """Error thrown if flash exe not executable"""
     try:
-        assemble = flash.Flash("LICENSE")
+        obj = flash.Flash("LICENSE")
     except NotExecutableError:
         return True
     else:
         return False
 
 
-def build_diff_cmd(infname1, infname2):
-    """Build a command-line for diff"""
-    cmd = ["diff",
-           infname1,
-           infname2]
-    build_diff_cmd = ' '.join(cmd)
-    return build_diff_cmd
-
-# cannot get the same result twice. 
-##def test_flash_exec():
-##    """Run flash on test data"""
-##    assemble = flash.Flash("flash")
-##    try:
-##        shutil.rmtree(OUTDIR)
-##    except FileNotFoundError:
-##        pass
-##    os.makedirs(OUTDIR, exist_ok=True)
-##    assemble.run("tests/test_data/DNAMIX_S95_L001_R1_001.fastq.gz",
-##                 "tests/test_data/DNAMIX_S95_L001_R2_001.fastq.gz",
-##                 4, "tests/test_out_flash/")
-##    target = os.path.join("tests", "test_targets",
-##                          "DNAMIX_S95_L001_flash.extendedFrags.fastq")
-##    result = os.path.join("tests", "test_out_flash",
-##                          "DNAMIX_S95_L001_flash.extendedFrags.fastq")
-##    with open(target, "r") as target_fh:
-##        with open(result, "r") as test_fh:
-##            assert_equal(target_fh.read(), test_fh.read())
+def test_flash_exec():
+    """Run flash on test data and compare output to precomputed target
+    this often does not produce the same result twice. So we may have
+    to disable this test"""
+    obj = flash.Flash("flash")
+    try:
+        shutil.rmtree(OUTDIR)
+    except FileNotFoundError:
+        pass
+    os.makedirs(OUTDIR, exist_ok=True)
+    result = obj.run(READS1, READS2, 4, OUTDIR, PREFIX, dry_run=False)
+    with open(TARGET, "r") as target_fh:
+        with open(result.outfileassembled, "r") as test_fh:
+            assert_equal(target_fh.read(), test_fh.read())
