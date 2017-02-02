2005476f5760339aeb7710c85d6a9813bd5f9b04
Peter Thorpe <peter.thorpe@hutton.ac.uk>
2017-02-02 14:50:32 +0000

    index and idxstats tests added

--- tests/test_wrapper_samtools_index.py
+++ tests/test_wrapper_samtools_index.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 
-"""Tests pycits wrapper for samtools_index."""
+"""Tests pycits wrapper for samtools_index.
+and IDXSTATS"""
 
 import os
 import shutil
 import gzip
 
 from pycits import samtools_index
+from pycits import samtools_idxstats
+
 from pycits.tools import NotExecutableError
 
 from nose.tools import nottest, assert_equal
@@ -14,6 +17,14 @@ from nose.tools import nottest, assert_equal
 # INPUT DATA
 INDIR = os.path.join("tests", "test_data", "samtools")
 BAM_FILE = os.path.join(INDIR, "Aligned.sortedByCoord.out.bam")
+
+OUTDIR = os.path.join("tests", "test_out_smatools_idxstats")
+
+OUTFILE = os.path.join(OUTDIR, "idxstats")
+
+# TARGET OUTPUT DATA
+TARGET = os.path.join("tests", "test_targets", "samtools",
+                      "MisMat_0_star_mappings")
 
 
 def test_samtools_index():
@@ -61,3 +72,66 @@ def test_samtools_index_exec():
     # test to see if it has produced the bai file.
     if not os.path.isfile(result.bai):
         return False
+
+
+def test_samtools_idxstats():
+    """Samtools_Idxstats instantiates with cmd-line if Samtools_Idxstats
+    is in $PATH"""
+    samtools_idxstats.Samtools_Idxstats("samtools")
+
+
+def test_samtools_idxstats_cmd():
+    """samtools_idxstats instantiates, runs and returns
+    correct form of cmd-line"""
+    # checking and making the output folder is staying in for now
+    if not os.path.exists(OUTDIR):
+        os.makedirs(OUTDIR)
+    obj = samtools_idxstats.Samtools_Idxstats("samtools")
+    target = ' '.join(["samtools",
+                       "idxstats",
+                       BAM_FILE,
+                       "|",
+                       "grep",
+                       '-v',
+                       "'*'",
+                       "|",
+                       "sort",
+                       "--reverse",
+                       "-n",
+                       "-k3",
+                       ">",
+                       OUTFILE])
+    stats = obj.run(BAM_FILE, OUTFILE)
+    assert_equal(stats.command,
+                 target)
+
+
+def test_samtools_idxstats_exec_notexist():
+    """Error thrown if samtools_idxstats executable does not exist"""
+    try:
+        obj = samtools_idxstats.Samtools_Idxstats(os.path.join
+                                                  (".", "samtools"))
+    except NotExecutableError:
+        return True
+    else:
+        return False
+
+
+def test_samtools_idxstats_notexec():
+    """Error thrown if samtools_idxstats exe not executable"""
+    try:
+        obj = samtools_idxstats.Samtools_Idxstats("LICENSE")
+    except NotExecutableError:
+        return True
+    else:
+        return False
+
+
+def test_samtools_idxstats_exec():
+    """Run samtools_idxstats on test data and compare output
+    to precomputed target"""
+    obj = samtools_idxstats.Samtools_Idxstats("samtools")
+    result = obj.run(BAM_FILE, OUTFILE)
+    with open(TARGET, "r") as target_fh:
+        with open(result.cov, "r") as test_fh:
+            assert_equal(target_fh.read(), test_fh.read())
