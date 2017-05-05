bb56d5e95cc36c8686228e5a939f8306deffcc9c
Leighton Pritchard <leighton.pritchard@hutton.ac.uk>
2017-05-05 17:41:22 +0100

    modified file locations for fq to fa conversion test

--- tests/test_tool_fq_fa.py
+++ tests/test_tool_fq_fa.py
@@ -3,36 +3,48 @@
 """Test program for a function in the module tools: convert_fq_to_fa
 """
 
+import gzip
 import os
 import shutil
 import subprocess
-from pycits.tools import NotExecutableError
-import gzip
+
 from nose.tools import nottest, assert_equal
 from pycits.tools import convert_fq_to_fa
+from pycits.tools import NotExecutableError
 
 
 # INPUT DATA LOCATION
 INDIR = os.path.join("tests", "test_data")
-OUTDIR = os.path.join("tests", "test_out_bio_fq_fa")
-TEST_FILE = "pear_test.assembled.fastq.gz"
-TEST_OUT = "converted_fq_fa.fasta"
+OUTDIR = os.path.join("tests", "test_out", "tools")
+INFILE = os.path.join(INDIR, "pear_test.assembled.fastq.gz")
+OUTFILE = os.path.join(OUTDIR, "converted_fq_fa.fasta")
+
 # folder checking
 if not os.path.exists(OUTDIR):
     os.makedirs(OUTDIR)
 
 # TARGET OUTPUT DATA
-TARGET = os.path.join("tests", "test_targets", "convert_fq_fa",
+TARGET = os.path.join("tests", "test_targets", "tools",
                       "pear_tests_FASTA_convert.fasta.gz")
+
+
+# The setup_outdir() function decorates functions by creating the appropriate
+# output directory tree
+def setup_outdir():
+    """Set up test fixtures"""
+    try:
+        shutil.rmtree(OUTDIR)
+    except FileNotFoundError:
+        pass
+    os.makedirs(OUTDIR, exist_ok=True)
 
 
 def test_convert_exec():
     """Run convert_fq_to_fa.py on test data and compare output to
     precomputed target
     """
-    result_file = os.path.join(OUTDIR, TEST_OUT)
-    convert_fq_to_fa((os.path.join(INDIR, TEST_FILE)), result_file)
+    convert_fq_to_fa(INFILE, OUTFILE)
 
     with gzip.open(TARGET, "rt") as target_fh:
-        with open(result_file, "r") as test_fh:
+        with open(OUTFILE, "r") as test_fh:
             assert_equal(target_fh.read(), test_fh.read())
